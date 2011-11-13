f21bc83c25d6c102a03849c05d80793bad062333
Fernando Graças <fgracas89@gmail.com>
2011-11-13 17:11:45 +0000

    layout da tab search finalized

--- gen/dspot/client/R.java
+++ gen/dspot/client/R.java
@@ -12,24 +12,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int close=0x7f020000;
-        public static final int expander_ic_minimized=0x7f020001;
-        public static final int facebook_icon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int ic_tab_favourites_selected=0x7f020004;
-        public static final int ic_tab_favourites_unselected=0x7f020005;
-        public static final int ic_tab_map_selected=0x7f020006;
-        public static final int ic_tab_map_unselected=0x7f020007;
-        public static final int ic_tab_me_selected=0x7f020008;
-        public static final int ic_tab_me_unselected=0x7f020009;
-        public static final int ic_tab_search_selected=0x7f02000a;
-        public static final int ic_tab_search_unselected=0x7f02000b;
-        public static final int profile_picture=0x7f02000c;
+        public static final int expander_down=0x7f020001;
+        public static final int expander_up=0x7f020002;
+        public static final int facebook_icon=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_tab_favourites_selected=0x7f020005;
+        public static final int ic_tab_favourites_unselected=0x7f020006;
+        public static final int ic_tab_map_selected=0x7f020007;
+        public static final int ic_tab_map_unselected=0x7f020008;
+        public static final int ic_tab_me_selected=0x7f020009;
+        public static final int ic_tab_me_unselected=0x7f02000a;
+        public static final int ic_tab_search_selected=0x7f02000b;
+        public static final int ic_tab_search_unselected=0x7f02000c;
+        public static final int profile_picture=0x7f02000d;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f050001;
         public static final int ScrollView1=0x7f050000;
-        public static final int TableLayout1=0x7f050022;
+        public static final int TableLayout1=0x7f050024;
+        public static final int content=0x7f050022;
+        public static final int expandableListView1=0x7f050026;
+        public static final int handle=0x7f050023;
         public static final int imageView1=0x7f050003;
+        public static final int listView1=0x7f050027;
         public static final int login_guestButton=0x7f05000f;
         public static final int login_loginButton=0x7f05000b;
         public static final int login_password=0x7f05000a;
@@ -45,8 +50,8 @@ public final class R {
         public static final int register_passwordConf=0x7f05001b;
         public static final int register_profileImage=0x7f05001f;
         public static final int register_username=0x7f050019;
-        public static final int relativeLayout1=0x7f050021;
-        public static final int seekBar1=0x7f050023;
+        public static final int seekBar1=0x7f050025;
+        public static final int slidingDrawer1=0x7f050021;
         public static final int tableLayout1=0x7f050004;
         public static final int tableLayout2=0x7f05000c;
         public static final int tableLayout3=0x7f050018;
--- src/dspot/client/SearchTab.java
+++ src/dspot/client/SearchTab.java
@@ -3,20 +3,39 @@ package dspot.client;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.database.DataSetObserver;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TableRow;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.SlidingDrawer.OnDrawerCloseListener;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
 
-public class SearchTab  extends ListActivity{
+public class SearchTab  extends Activity implements OnDrawerOpenListener, OnDrawerCloseListener{
 	
-	Api api;
+	private Api api;
+	private ListView lv = null;
+	private ExpandableListView elv = null;
 	
-	 static final String[] Options = new String[] {"Search Near Me", "Search By Location", "Last Search", "Search By Sports", ""};
+	static final String[] Options = new String[] {"Search Near Me", "Search By Location", "Last Search"};
+	
+	
+	private String[] groups = { "Sports" };
+    private String[][] children = {{"BasketBall", "Joggin", "Running", "Football"}};
+
+	static final String[] Sports = new String[] {"BasketBall", "Joggin", "Running", "Football"};
     
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +44,155 @@ public class SearchTab  extends ListActivity{
 		
 		setContentView(R.layout.tab_search);
 		
-		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Options));
-	    	  
+		lv = (ListView)findViewById(R.id.listView1);
+		elv = (ExpandableListView)findViewById(R.id.expandableListView1);
+		
+		lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Options));
+	    elv.setAdapter(new ExpandableListAdapter() {
+			
+			@Override
+			public void unregisterDataSetObserver(DataSetObserver observer) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void registerDataSetObserver(DataSetObserver observer) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onGroupExpanded(int groupPosition) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onGroupCollapsed(int groupPosition) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean isEmpty() {
+				return false;
+			}
+			
+			@Override
+			public boolean isChildSelectable(int groupPosition, int childPosition) {
+				return true;
+			}
+			
+			@Override
+			public boolean hasStableIds() {
+				return true;
+			}
+			
+			@Override
+			public View getGroupView(int groupPosition, boolean isExpanded,
+					View convertView, ViewGroup parent) {
+				TextView textView = getGenericView();
+	            textView.setText(getGroup(groupPosition).toString());
+	            textView.setTextSize(25);
+	            return textView;
+			}
+			
+			@Override
+			public long getGroupId(int groupPosition) {
+				return groupPosition;
+			}
+			
+			@Override
+			public int getGroupCount() {
+				return groups.length;
+			}
+			
+			@Override
+			public Object getGroup(int groupPosition) {
+				return groups[groupPosition];
+			}
+			
+			@Override
+			public long getCombinedGroupId(long groupId) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public long getCombinedChildId(long groupId, long childId) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public int getChildrenCount(int groupPosition) {
+				return children[groupPosition].length;
+			}
+			
+			
+			public TextView getGenericView() {
+	            // Layout parameters for the ExpandableListView
+	            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+	                    ViewGroup.LayoutParams.MATCH_PARENT, 64);
+
+	            TextView textView = new TextView(SearchTab.this);
+	            textView.setLayoutParams(lp);
+	            // Center the text vertically
+	            textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+	            // Set the text starting position
+	            textView.setPadding(36, 0, 0, 0);
+	            return textView;
+	        }
+			
+			
+			@Override
+			public View getChildView(int groupPosition, int childPosition,
+					boolean isLastChild, View convertView, ViewGroup parent) {
+				TextView textView = getGenericView();
+	            textView.setText(getChild(groupPosition, childPosition).toString());
+	            textView.setTextSize(15);
+	            textView.setPadding(60, 0,0,0);
+	            return textView;
+			}
+			
+			@Override
+			public long getChildId(int groupPosition, int childPosition) {
+				return childPosition;
+			}
+			
+			@Override
+			public Object getChild(int groupPosition, int childPosition) {
+				return children[groupPosition][childPosition];
+			}
+			
+			@Override
+			public boolean areAllItemsEnabled() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+	    
+	    lv.setOnItemClickListener(new ListView.OnItemClickListener() {
+			
+	    	@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+	    		Toast toast = Toast.makeText(getApplicationContext(), "AKI", Toast.LENGTH_SHORT);
+	    		toast.show();
+				
+			}
+	    });
+	    
+	    
+	    ((SlidingDrawer)findViewById(R.id.slidingDrawer1)).setOnDrawerOpenListener(this);
+	    ((SlidingDrawer)findViewById(R.id.slidingDrawer1)).setOnDrawerCloseListener(this);
+	    
+	    
+	    
+	    Toast toast = Toast.makeText(getApplicationContext(), "Click the icon below, to costumize the search", Toast.LENGTH_LONG);
+	    toast.show();
     }
-	
-	
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		String item = (String) getListAdapter().getItem(position);
-		Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-	}
 	
 	
 	public void searchByLocation(){
@@ -84,21 +242,35 @@ public class SearchTab  extends ListActivity{
             // do something when the button is clicked
             public void onClick(DialogInterface arg0, int arg1) {
             	
-            	
             	if(api.logout()){
             		finish();
             	}else{
             		Toast toast = Toast.makeText(getApplicationContext(), "Logout failed", Toast.LENGTH_SHORT);
             		toast.show();
-            	}
-            	
-            	
-                
+            	}        
             }
         });
 
         // display box
         alertbox.show();
+
+	}
+
+
+	@Override
+	public void onDrawerClosed() {
+		// TODO Auto-generated method stub
+		lv.setVisibility(ListView.VISIBLE);
+		((ImageView)findViewById(R.id.handle)).setImageResource(R.drawable.expander_up);
+		
+	}
+
+
+	@Override
+	public void onDrawerOpened() {
+		// TODO Auto-generated method stub
+		lv.setVisibility(ListView.GONE);
+		((ImageView)findViewById(R.id.handle)).setImageResource(R.drawable.expander_down);
 
 	}
 
