efbc9faf946cfdc8f948571df21b0345d920360d
Fernando Graças <fgracas89@gmail.com>
2011-11-14 11:47:41 +0000

    Base de dados criada

--- src/dspot/client/database/DatabaseAdapter.java
+++ src/dspot/client/database/DatabaseAdapter.java
@@ -1,5 +1,102 @@
 package dspot.client.database;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class DatabaseAdapter {
+	
+	private Context context;
+	private static SQLiteDatabase database;
+	private static DatabaseHelper dbHelper;
+	
+	
+	public DatabaseAdapter(Context context) {
+		this.context = context;
+	}
+
+	public DatabaseAdapter open() throws SQLException {
+		dbHelper = new DatabaseHelper(context);
+		database = dbHelper.getWritableDatabase();
+		return this;
+	}
+
+	public void close() {
+		dbHelper.close();
+	}
+	
+	
+	public long createLocation(int id, String name) {
+		
+		ContentValues initialvalues = new ContentValues();
+		initialvalues.put("id", id);
+		initialvalues.put("location", name);
+
+		return database.insert("locations", null, initialvalues);
+	}
+	
+	
+	public long createSport(int id, String name) {
+		
+		ContentValues initialvalues = new ContentValues();
+		initialvalues.put("id", id);
+		initialvalues.put("sport", name);
+
+		return database.insert("sports", null, initialvalues);
+	}
+	
+	
+	public Map<String, Integer> getLocations(){
+		
+		Map<String, Integer> locations = new TreeMap<String, Integer>();
+		
+		String selectLocations = "Select * from locations";  
+		
+	 	Cursor locationsCursor = database.rawQuery(selectLocations, null);
+	 	
+	 	locationsCursor.moveToFirst();
+	 	if(locationsCursor.getCount() == 0){
+	 		locationsCursor.close();
+	 		return locations;
+	 	}
+	 	
+	 	locationsCursor.moveToFirst();
+	 	do {
+	 		locations.put(locationsCursor.getString(1), locationsCursor.getInt(0));
+	 	}while(locationsCursor.moveToNext());
+	 	locationsCursor.close();
+	 	
+	 	return locations;
+	}
+	
+	
+	
+	public Map<String, Integer> getSports(){
+		
+		Map<String, Integer> sports = new TreeMap<String, Integer>();
+		
+		String selectVersion = "Select * from sports";  
+		
+	 	Cursor sportsCursor = database.rawQuery(selectVersion, null);
+	 	
+	 	sportsCursor.moveToFirst();
+	 	if(sportsCursor.getCount() == 0){
+	 		sportsCursor.close();
+	 		return sports;
+	 	}
+	 	
+	 	sportsCursor.moveToFirst();
+	 	do {
+	 		sports.put(sportsCursor.getString(1), sportsCursor.getInt(0));
+	 	}while(sportsCursor.moveToNext());
+	 	sportsCursor.close();
+	 	
+	 	return sports;
+	}
 
 }
--- src/dspot/client/database/DatabaseHelper.java
+++ src/dspot/client/database/DatabaseHelper.java
@@ -1,5 +1,40 @@
 package dspot.client.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+	
+	private static final String DATABASE_NAME = "dspot.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	private static final String DATABASE_CREATE_SPORTS = " CREATE TABLE sports (id INTEGER PRIMARY KEY,  sport TEXT)";
+	private static final String DATABASE_CREATE_LOCATIONS = " CREATE TABLE locations (id INTEGER PRIMARY KEY,  location TEXT)";
+
+
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE_SPORTS);
+		database.execSQL(DATABASE_CREATE_LOCATIONS);
+	}
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+		Log.w(DatabaseHelper.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		
+		database.execSQL(" DROP TABLE IF EXISTS locations");
+		database.execSQL(" DROP TABLE IF EXISTS sports");
+
+		onCreate(database);
+	}
 
 }
