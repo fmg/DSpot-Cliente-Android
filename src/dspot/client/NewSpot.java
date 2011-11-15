82f79e6b11994f22eb3c9593b0548d3fb7fd8853
Fernando Graças <fgracas89@gmail.com>
2011-11-15 00:27:11 +0000

    Actividade de criar novo spot quase concluida

--- src/dspot/client/NewSpot.java
+++ src/dspot/client/NewSpot.java
@@ -1,15 +1,23 @@
 package dspot.client;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.AlteredCharSequence;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NewSpot extends Activity{
 	
 	private Api api;
-	private static final int CAMERA_PIC_REQUEST = 1337;  
+	private static final int CAMERA_PIC_REQUEST = 1337;
+	AlertDialog alert;  
 
 	
 	@Override
@@ -19,11 +27,62 @@ public class NewSpot extends Activity{
 		api = ((Api)getApplicationContext());
 		
 		setContentView(R.layout.new_spot);
+		
+		((ImageView)findViewById(R.id.new_spot_picture)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				takePicture();
+				
+			}
+		});
+		
+		
+		Toast toast = Toast.makeText(getApplicationContext(), "Tap the camera icon to take picture", Toast.LENGTH_SHORT);
+		toast.show();
+		
+		((Button)findViewById(R.id.new_spot_setSports)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				setSports();
+			}
+		});
+		
+		
+	}
+	
+	
+	public void setSports(){
+		final CharSequence[] items = {"Red", "Green", "Blue"};
+		boolean[] checkeditems = {false, false, false};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Pick a color");
+		builder.setNeutralButton("ok", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				alert.dismiss();
+				
+			}
+		});
+		builder.setMultiChoiceItems(items,checkeditems, new DialogInterface.OnMultiChoiceClickListener(){
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which,
+					boolean isChecked) {
+				Toast.makeText(getApplicationContext(), items[which], Toast.LENGTH_SHORT).show();
+			}
+		});
+		alert = builder.create();
+		alert.show();
 	}
 	
 	
 	
-	public void nada(){
+	
+	public void takePicture(){
 		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 		startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);  
 
@@ -35,11 +94,12 @@ public class NewSpot extends Activity{
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
 	    if (requestCode == CAMERA_PIC_REQUEST) {  
 	        // do something  
-	    	Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
-	    	
-	    	//ImageView image = (ImageView) findViewById(R.id.photoResultView);  
-	    	//image.setImageBitmap(thumbnail);
-
+	    	if(data.hasExtra("data")){
+		    	Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
+		    	
+		    	ImageView image = (ImageView) findViewById(R.id.new_spot_picture);  
+		    	image.setImageBitmap(thumbnail);
+	    	}
 	    }  
 	}  
 	
